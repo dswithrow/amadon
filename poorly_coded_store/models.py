0a91a3abfcc8f27e9bd5ed88d840243569cf6213
dswithrow <withrow.dylan@gmail.com>
2020-09-13 16:31:34 -0700

    Corrected code

--- poorly_coded_store/models.py
+++ poorly_coded_store/models.py
@@ -9,5 +9,6 @@ class Product(models.Model):
 class Order(models.Model):
     quantity_ordered = models.IntegerField()
     total_price = models.DecimalField(decimal_places=2, max_digits=6)
+    lifetime_spent = models.DecimalField(decimal_places=2, max_digits=8, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
 
 def index(request):
@@ -7,10 +7,20 @@ def index(request):
     }
     return render(request, "store/index.html", context)
 
-def checkout(request):
+def process(request):
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
+    price_from_form = float(Product.objects.get(id=request.POST["pid"]).price)
     total_charge = quantity_from_form * price_from_form
     print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    prev_order = 0.00
+    try:
+        prev_order = float(Order.objects.last().lifetime_spent)
+    finally:
+        Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge, lifetime_spent=prev_order+total_charge)
+    return redirect("/checkout")
+
+def checkout(request):
+    context = {
+        "order": Order.objects.last()
+    }
+    return render(request, "store/checkout.html", context)
